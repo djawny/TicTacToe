3335fb492da97fe24843c8b8ab56e09bdb84b33e
djawny <djawny@op.pl>
2017-02-03 21:41:09 +0100

    zmiana 03/02

--- src/main/java/sda/GameLogic.java
+++ src/main/java/sda/GameLogic.java
@@ -1,16 +1,16 @@
 package sda;
 
 import javax.swing.*;
+import java.util.stream.IntStream;
 
 public class GameLogic {
     public boolean checkWinCondition(JButton[][] gameBoardButtons) {
-
-        for (int i = 0; i < gameBoardButtons.length; i++) {
-            if (checkColumnForWinning(gameBoardButtons[i]) || checkRowForWinning(gameBoardButtons, i)) {
-                return true;
-            }
-        }
-        return checkDiagonals(gameBoardButtons);
+        return IntStream
+                .range(0, gameBoardButtons.length)
+                .filter(i -> checkColumnForWinning(gameBoardButtons[i]) || checkRowForWinning(gameBoardButtons, i))
+                .findAny()
+                .isPresent()
+                || checkDiagonals(gameBoardButtons);
     }
 
     private boolean checkColumnForWinning(JButton[] gameBoardButtons) {
